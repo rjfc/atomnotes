87794194f5e303c350f81f82615e7318ec975df5
rjfc <russelljfchan@gmail.com>
2017-05-19 13:34:32 -0400

    Added dark overlay on sign up and log in button click

---
+++ public/scripts/main.js
@@ -0,0 +1,11 @@
+$(".dark-overlay").click(function() {
+    $(this).fadeOut(20);
+});
+
+$(".btn-sign-up").click(function() {
+    $(".dark-overlay").fadeIn(20);
+});
+
+$(".btn-log-in").click(function() {
+    $(".dark-overlay").fadeIn(20);
+});
