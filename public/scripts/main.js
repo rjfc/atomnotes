4fbb6b0e34cf4fbd4066962a2ed6d7919e6ec9c2
rjc <russelljfchan@gmail.com>
2017-05-24 00:43:55 -0400

    Initiated sign up and log in popup boxes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,20 @@
 $(".dark-overlay").click(function() {
     $(this).fadeOut(20);
+    $("#popup-sign-up").fadeOut(20);
 });
+
+$(".popup-close").click(function() {
+    $(this).parent().parent().fadeOut(20);
+    $(".dark-overlay").fadeOut(20);
+});
+
 
 $(".btn-sign-up").click(function() {
     $(".dark-overlay").fadeIn(20);
+    $("#popup-sign-up").fadeIn(20);
 });
 
 $(".btn-log-in").click(function() {
     $(".dark-overlay").fadeIn(20);
+    $("#popup-log-in").fadeIn(20);
 });
