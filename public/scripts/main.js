8222cdd1dc56471fba4f5fd4f4758b7229b7d174
rjc <russelljfchan@gmail.com>
2017-05-24 20:24:34 -0400

    Finished version 1 of sign up and log in boxes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,7 @@
 $(".dark-overlay").click(function() {
     $(this).fadeOut(20);
     $("#popup-sign-up").fadeOut(20);
+    $("#popup-log-in").fadeOut(20);
 });
 
 $(".popup-close").click(function() {
@@ -8,13 +9,14 @@ $(".popup-close").click(function() {
     $(".dark-overlay").fadeOut(20);
 });
 
-
 $(".btn-sign-up").click(function() {
     $(".dark-overlay").fadeIn(20);
     $("#popup-sign-up").fadeIn(20);
+    $("#popup-sign-up-form-email").focus();
 });
 
 $(".btn-log-in").click(function() {
     $(".dark-overlay").fadeIn(20);
     $("#popup-log-in").fadeIn(20);
+    $("#popup-log-in-form-email").focus();
 });
